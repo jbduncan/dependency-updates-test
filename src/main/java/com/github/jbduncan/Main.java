2529c321dfb40a1f642079e11d262fac567d790f
Jonathan Bluett-Duncan <jbluettduncan@gmail.com>
2017-08-04 15:57:45 +0100

    Initial commit

---
+++ src/main/java/com/github/jbduncan/Main.java
@@ -0,0 +1,9 @@
+package com.github.jbduncan;
+
+import static com.google.common.base.Strings.repeat;
+
+public final class Main {
+  public static void main(String[] args) {
+    System.out.println(repeat("Hello, world! ", 3));
+  }
+}
